122754f4bad93fe879ff17c057e04c46a95da501
Stanley Ou <stanleyo@andrew.cmu.edu>
2023-08-14 18:16:16 -0400

    Baseline LSTM neural network.

--- main.py
+++ main.py
@@ -14,6 +14,83 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 from datetime import timedelta, datetime as dt
 
+import tensorflow as tf
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow.keras.layers as layers
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.metrics import r2_score
+
+
+
+def create_window(data, predict_days=30):
+    X_data, y_data = [], []
+    #Using 30 days at a time to predict next val
+    print(len(data))
+    for x in range(predict_days, len(data)):
+        X_data.append(data[x - predict_days: x])
+        y_data.append(data[x])
+    X_data, y_data = np.array(X_data), np.array(y_data)
+    X_data = np.reshape(X_data, (X_data.shape[0], 1, X_data.shape[1]))
+    return X_data, y_data
+
+#Creates a neural network using the stock history data passed in. Last 180 days used - 120 for training, 60 for testing model
+def test_Model(data : object) -> object:
+    train_days = 120
+    test_days = 60
+
+    data = data.iloc[-(train_days + test_days):,]
+    data = data.drop(set(data.columns).difference(['Close']), axis=1)
+
+    test_data = np.reshape(data.iloc[-test_days:,0], (-1,1))
+    train_data = np.reshape(data.iloc[-train_days - test_days: -test_days,0], (-1,1))
+
+    #Transforming data
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaler.fit(train_data)
+    train_data = scaler.transform(train_data)
+    test_data = scaler.transform(test_data)
+
+    # print(train_data)
+
+    #Using 10 days at a time to predict next val
+    predict_days = 10
+    X_train, y_train = create_window(train_data, predict_days)
+    X_test, y_test = create_window(test_data, predict_days)
+
+    print(X_train.shape, X_test.shape)
+    # X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+    # X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+
+    #Building model
+    model = tf.keras.Sequential()
+    model.add(LSTM(units = 128, activation='relu', input_shape = (1, predict_days)))
+    model.add(Dropout(0.2))
+    model.add(Dense(units = 64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units = 32, activation= 'relu'))
+    model.add(Dense(units = 16, activation= 'relu'))
+    model.add(Dense(units=1, activation='relu'))
+
+    model.compile(loss='mae', optimizer='adam')
+    model.fit(X_train, y_train, epochs=50)
+    print(model.summary())
+
+    #Predictions
+    # train_preds = np.reshape(model.predict(X_train), (X_train[1], 1))
+    # test_preds = np.reshape(model.predict(X_test), (X_test[1], 1))
+
+    train_preds = model.predict(X_train)
+    test_preds = model.predict(X_test)
+
+    print(y_test.shape, test_preds.shape)
+    print(y_train.shape, train_preds.shape)
+    print(r2_score(train_preds, y_train), r2_score(test_preds, y_test))
+
+    return model
+
+
 
 #Class definitions
 # Stock Class:
@@ -45,7 +122,7 @@ class Stock:
     def get_Profit(self):
         return self.get_Curr_Value() - self.get_Start_Value
     
-    def visualize_Stock(self):
+    def visualize_Stock(self) -> None:
         dat = self.data.reset_index()
         plt.title(f"{self.ticker} Stock Performance")
         sns.lineplot(x="Date", y="Close", data = dat[dat["Date"] >= self.date_added.strftime('%Y-%m-%d')],
@@ -149,14 +226,9 @@ class Portfolio:
                     #     st = self.get_stock(input("Stock not in portfolio, enter again:"))
                     st.visualize_Stock()
         pass
-
+"""
 line = None
 pf = Portfolio()
-
-# apple = pd.read_csv('AppleData.csv')
-# #print(apple.reset_index())
-# print(apple)
-# #print(apple[apple["Date"] >= "2020-01-01"])
 
 #Continuously asks for stock input until user is done
 while line != "Done":
@@ -186,10 +258,14 @@ while line != "Done":
     pf.add_Stock(stock)
     pf.print_Transactions()
 
-print(pf.get_stock("AAPL"))
 
 pf.visualize_Profits()
+"""
 
-pf.process_Request()
+data = yf.download('aapl')
+
+mod = test_Model(data)
+
+# pf.process_Request()
     
 
