107f8ef29e52c333e73e9fc0f05768570f8635c7
Stanley Ou <stanleyo@andrew.cmu.edu>
2023-08-15 17:41:03 -0400

    Visualzied predictions, added pred of next day.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """
 Stock portfolio tracker with visualization and predictive capabilities.
 
-
 - Limitation: Adding to an existing stock
 
 """
@@ -21,8 +20,7 @@ import tensorflow.keras.layers as layers
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.metrics import r2_score
 
-
-
+# Prepares data used to fit model
 def create_window(data, predict_days=30):
     X_data, y_data = [], []
     #Using 30 days at a time to predict next val
@@ -34,8 +32,29 @@ def create_window(data, predict_days=30):
     X_data = np.reshape(X_data, (X_data.shape[0], 1, X_data.shape[1]))
     return X_data, y_data
 
+def create_Model(max_units, num_Dense, num_LSTM, timeframe, active_func = 'None'):
+    #Building model
+    model = tf.keras.Sequential()
+
+    model.add(LSTM(units = max_units, activation=active_func, return_sequences=True, input_shape = (1, timeframe)))
+    model.add(Dropout(0.2))
+    for i in range(num_LSTM):
+        if i == num_LSTM - 1:
+            model.add(LSTM(units = max_units//2, activation=active_func, return_sequences=False))
+        else: 
+            model.add(LSTM(units = max_units//2, activation=active_func, return_sequences=True))
+            model.add(Dropout(0.2))
+
+    for i in range(num_Dense):
+        model.add(Dense(units = max_units//(2**i), activation=active_func))
+    model.add(Dense(units=1))
+
+    return model
+
 #Creates a neural network using the stock history data passed in. Last 180 days used - 120 for training, 60 for testing model
-def test_Model(data : object) -> object:
+def get_Predictions(data : object) -> object:
+    #Using 10 days at a time to predict next val, train data size, test data size
+    predict_days = 10
     train_days = 120
     test_days = 60
 
@@ -44,51 +63,99 @@ def test_Model(data : object) -> object:
 
     test_data = np.reshape(data.iloc[-test_days:,0], (-1,1))
     train_data = np.reshape(data.iloc[-train_days - test_days: -test_days,0], (-1,1))
+    predict_data = np.reshape(data.iloc[-predict_days:, 0], (-1,1))
 
     #Transforming data
     scaler = MinMaxScaler(feature_range=(0,1))
     scaler.fit(train_data)
+
     train_data = scaler.transform(train_data)
     test_data = scaler.transform(test_data)
+    predict_data = scaler.transform(predict_data)
 
-    # print(train_data)
-
-    #Using 10 days at a time to predict next val
-    predict_days = 10
     X_train, y_train = create_window(train_data, predict_days)
     X_test, y_test = create_window(test_data, predict_days)
 
-    print(X_train.shape, X_test.shape)
-    # X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
-    # X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+    def hyperparameterize():
+        #Hyperparameterization
+        r2scores = {}
+        max_nodes = [64, 96, 128, 256]
+        num_Dense = range(3)
+        num_LSTM = range(1, 4)
+        activations = [None, 'relu']
+        epoch_list = [10, 30, 50]
 
+        for n in max_nodes:
+            for d in num_Dense:
+                for l in num_LSTM:
+                    for a in activations:
+                        for e in epoch_list:
+                            model = create_Model(n, d, l, predict_days, a)
+                            model.compile(loss='mae', optimizer='adam')
+                            model.fit(X_train, y_train, epochs=e)
+                            test_preds = model.predict(X_test)
+                            r2scores[f"nodes: {n}, dense: {d}, lstm: {l}, {a}, epoch: {e}"] = r2_score(test_preds, y_test)
+                            
+        r2scores = {k: v for k, v in sorted(r2scores.items(), key=lambda item: item[1])}
+        print(r2scores)
 
-    #Building model
-    model = tf.keras.Sequential()
-    model.add(LSTM(units = 128, activation='relu', input_shape = (1, predict_days)))
-    model.add(Dropout(0.2))
-    model.add(Dense(units = 64, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(units = 32, activation= 'relu'))
-    model.add(Dense(units = 16, activation= 'relu'))
-    model.add(Dense(units=1, activation='relu'))
+    # from hyperparameter optimization: nodes: 128, dense: 2, lstm: 2, None, epoch: 50': 0.6777185615111131
+    r2scores=[]
+    # for i in range(100):
 
+    model = create_Model(128, 3, 2, predict_days, None)
     model.compile(loss='mae', optimizer='adam')
     model.fit(X_train, y_train, epochs=50)
-    print(model.summary())
+    test_preds = model.predict(X_test)
+    r2scores.append( r2_score(test_preds, y_test))
 
-    #Predictions
-    # train_preds = np.reshape(model.predict(X_train), (X_train[1], 1))
-    # test_preds = np.reshape(model.predict(X_test), (X_test[1], 1))
+    print(sorted(r2scores), sum(r2scores)/len(r2scores))
+    #Predictions + Plotting
 
     train_preds = model.predict(X_train)
-    test_preds = model.predict(X_test)
+    # test_preds = model.predict(X_test)
+    # print(r2_score(train_preds, y_train), r2_score(test_preds, y_test))
 
-    print(y_test.shape, test_preds.shape)
-    print(y_train.shape, train_preds.shape)
-    print(r2_score(train_preds, y_train), r2_score(test_preds, y_test))
+    #Graphing Predictions on Train + Testing Data
 
-    return model
+    train_prices = scaler.inverse_transform(train_preds)
+    test_prices = scaler.inverse_transform(test_preds)
+
+    train_index = data.index[-train_days - test_days + predict_days : -test_days]
+    test_index = data.index[-test_days + predict_days:]
+
+    train_prices_series = pd.Series(train_prices.flatten(), index = train_index)
+    test_prices_series = pd.Series(test_prices.flatten(), index = test_index)
+
+    train_data, test_data = data.copy(), data.copy()
+    train_data.columns, test_data.columns = ['Actual Price'], ['Actual Price']
+    train_data = train_data.merge(right=train_prices_series.rename("Train Predictions"), left_index = True, right_index = True)
+    test_data = test_data.merge(right=test_prices_series.rename("Test Predictions"), left_index = True, right_index = True)
+     
+    plt.style.use('ggplot')
+    fig, axes = plt.subplots(1,2, sharey = True, figsize=(12,8))
+    fig.suptitle("Predictions on Train Set (left) and Test Set (right)")
+
+    axes[0].set_title("Model Predictions on Training Data")
+    sns.lineplot(ax = axes[0], data=train_data, x = train_data.index, y = "Actual Price", label="Actual Price")
+    sns.lineplot(ax = axes[0], data=train_data, x = train_data.index, y = "Train Predictions", label = "Predicted Price")
+
+    axes[1].set_title("Model Predictions on Testing Data")
+    sns.lineplot(ax = axes[1], data=test_data, x = test_data.index, y = "Actual Price", label="Actual Price")
+    sns.lineplot(ax = axes[1], data=test_data, x = test_data.index, y = "Test Predictions", label = "Predicted Price")
+
+    plt.savefig("StockPredictions.jpg")
+    plt.show()
+
+    #Getting predictions for the next day
+    predict_data = np.array(predict_data)
+    predict_data = np.reshape(predict_data, (1, 1, predict_data.shape[0]))
+    pred = model.predict(predict_data)
+
+    pred = scaler.inverse_transform(pred)
+    print(pred[0])
+
+    return pred[0]
 
 
 
@@ -129,9 +196,8 @@ class Stock:
                      err_style="bars")
         plt.show()
         pass
-    
-    def predict(self):
-        pass
+    def make_Predictions(self) -> float:
+        return get_Predictions(self.data)
 
 class Portfolio:
     def __init__(self, stock_list = [], transactions = "", net_worth = 0):
@@ -194,9 +260,11 @@ class Portfolio:
         print(profits_df)
         
         plt.style.use('ggplot')
+        plt.figure(figsize=(16,12))
         plt.title("Portfolio Net Profit by Time")
         graph_ = sns.lineplot(data=profits_df, x=profits_df.index, y="Total Profits", c='Green')
         graph_.xaxis.set_major_locator(ticker.LinearLocator(5))
+        plt.savefig("Portfolio_Profits.jpg")
         plt.show()
 
         pass
@@ -208,25 +276,29 @@ class Portfolio:
     def process_Request(self):
         while (1):
             print("Enter your request. For a list of options enter 'Help': ")
-            line = input()
+            line = input().lower()
             
             match line:
-                case "Help":
+                case "help":
                     pass
-                case "History":
+                case "predict":
+                    req = input("Enter a stock ticker: \n")
+                    st = self.get_stock(req)
+                    print(st.make_Predictions())
+                case "history":
                     self.print_Transactions()
-                case "Profit":
+                case "profit":
                     print("$ " + str(self.calc_Net_Profit()))
-                case "Q":
+                case "q":
                     return
-                case "V":
+                case "v":
                     req = input("Enter a stock ticker: \n")
                     st = self.get_stock(req)
                     # while not st is None:
                     #     st = self.get_stock(input("Stock not in portfolio, enter again:"))
                     st.visualize_Stock()
         pass
-"""
+
 line = None
 pf = Portfolio()
 
@@ -258,14 +330,8 @@ while line != "Done":
     pf.add_Stock(stock)
     pf.print_Transactions()
 
-
 pf.visualize_Profits()
-"""
 
-data = yf.download('aapl')
-
-mod = test_Model(data)
-
-# pf.process_Request()
+pf.process_Request()
     
 
