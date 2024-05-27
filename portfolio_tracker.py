160f4369e6c9904ae30d3c1101c9d6b493a38c4c
Stanley Ou <stanleyo@andrew.cmu.edu>
2024-05-26 23:19:03 -0400

    Modified object representation of stocks

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -13,6 +13,8 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 from datetime import timedelta, datetime as dt
 from modeling import get_Predictions
+from typing import List, Dict, Tuple, Union, Optional, Any, Callable, Iterable
+
 
 #Class definitions
 # Stock Class:
@@ -22,30 +24,68 @@ date_added - datetime object
 
 """
 class Stock:
-    def __init__(self, ticker: str, date_added: object, data, share_count: float):
-        self.ticker = ticker
-        self.date_added = date_added #List of datetimes to represent each addition
-        self.data = data # Dataframe of 
-        self.share_count = share_count
-        
+    def __init__(self, ticker: str, date_shares: List[Tuple[object, float]], data: object):
+        self.ticker = ticker 
+        self.date_shares = date_shares #List of (datetime, float) to represent each addition
+        self.data = data # Pandas dataframe using yfinance data
+
     def __eq__(self, __value: object) -> bool:
+        """
+        Two stocks are equal if they have the same ticker.
+        """
         return self.ticker == __value.ticker
 
     def __str__(self) -> str:
-        return f"{self.share_count} shares of {self.ticker} added on {self.date_added.strftime('%Y-%m-%d')}."
+        """
+        Prints out the information about the stock pertaining to the portfolio.
+        """
+        res = ""
+        for date_added, share_count in self.date_shares: 
+            res += f"{share_count} shares of {self.ticker} added on {date_added.strftime('%Y-%m-%d')}. \n"
+
+        return res
     
-    def get_Start_Value(self) -> float:
-        return float(self.data.loc[self.date_added].iloc[3]) * float(self.share_count)
+    def get_Start_Value(self) -> List[float]:
+        """
+        Gets the prices of the stock when purchased iniitally at each date. 
+        """
+        res = []
+        for share_count, date_added in self.date_shares:
+            res.append(float(self.data.loc[date_added].iloc[3]) * float(share_count))
+        return res
 
-    def get_Curr_Value(self) -> float:
-        return float(self.data.iloc[-1][3]) * self.share_count
+    def get_Curr_Value(self) -> List[float]:
+        """
+        Gets current price of the stock, given number of shares. 
+        """
 
-    #Returns net profit/loss from this stock 
-    def get_Profit(self):
-        return self.get_Curr_Value() - self.get_Start_Value
+        curr_price = float(self.data.iloc[-1][3])   
+        return curr_price * self.getShares()
+
+    def get_Profit(self) -> float:
+        """
+        Returns net profit/loss from this stock. 
+        """
+        shares = [share_count for _, share_count in self.date_shares]
+
+        sum = 0
+        for v, s in zip(self.get_Start_Value(), shares):
+            res += v*s
+
+        return sum - self.get_Curr_Value()
+    
+    def addShares(self, date: str, num_shares) -> None:
+        self.date_shares.append((dt.strptime(date, "'%Y-%m-%d'"), num_shares))
+    
+    def getShares(self) -> float:
+        return sum([share_count for _, share_count in self.date_shares])
     
     def visualize_Stock(self) -> None:
+        """
+        Creates line graph of stock's performance.
+        """
         dat = self.data.reset_index()
+
         plt.title(f"{self.ticker} Stock Performance")
         sns.lineplot(x="Date", y="Close", data = dat[dat["Date"] >= self.date_added.strftime('%Y-%m-%d')],
                      err_style="bars")
@@ -57,13 +97,17 @@ class Stock:
 
 class Portfolio:
     def __init__(self, stock_list = [], transactions = "", net_worth = 0):
-        self.stock_list = stock_list
-        self.transactions = transactions
-        self.net_worth = net_worth
+        self.stock_list = stock_list # List of stock objects 
+        # self.inactive_stocks = []
+        self.transactions = transactions # List of transactions 
+        self.net_worth = net_worth # Total assets invested in the portfolio 
 
     # Input: Stock Ticker 
     # Return None if stock is not in portfolio
     def get_stock(self, stock_ticker: str) -> Stock:
+        """
+        Retrieves stock if exists in portfolio, else returns None.
+        """
         stock_ticker = stock_ticker.upper()
         for st in self.stock_list:
             if st.ticker == stock_ticker :
@@ -71,17 +115,56 @@ class Portfolio:
         return None
     
     #Adds stock to portfolio, updates transaction history
-    def add_Stock(self, stock) -> None:
-        st = self.get_stock(stock.ticker)
+    def add_Stock(self, tick: str, date: str, num_shares: float) -> None:
+        st = self.get_stock(tick)
+        invest_dt = dt.strptime(date, "%Y-%m-%d")
+
         if st:
-            st.share_count += stock.share_count
-        else:    
-            self.stock_list.append(stock)
+            st.date_shares.append((invest_dt, num_shares))
+            pass
+    
+        #Gets stock data from yfinance
+        tick_data = yf.download(tick)
+        # print(tick_data)
+
+        #Check if date is valid in data
+        while not invest_dt.strftime("%Y-%m-%d") in tick_data.iloc[:, 0] :
+            invest_dt += timedelta(days = 1)
+            #print(f"Date added changed to {}")
+
+        stock = Stock(tick, [(invest_dt, num_shares)], tick_data)   
+        self.stock_list.append(stock)
+
         self.transactions += ("\n" + str(stock))
+
+        self.net_worth += stock.get_Curr_Value()
+
         pass
 
-    def remove_Stock(self, stock, num_shares) -> None:
-        pass
+    def remove_Stock(self, tick: str, date: str, num_shares: float = None) -> None:
+        """
+        Removes num_shares of a stock from the portfolio. Removes all shares if num_shares is not specified. 
+        Removed stock shares are represented by negative share counts. 
+        """
+        st = self.get_stock(tick)
+
+        if not st:
+            print(f"ERROR: Stock {tick} not found in portfolio.")
+            pass
+
+        elif not num_shares:
+            st.addShares(tick, date, -st.getShares())
+            self.net_worth -= st.get_Curr_Value()
+            pass
+
+        elif num_shares > st.getShares():
+            print(f"ERROR: Removing {num_shares} of {tick} but portfolio only contains {st.getShares()}")
+            pass
+
+        else: 
+            self.net_worth -= (st.get_Curr_Value() / st.getShares) * num_shares
+            st.addShares(tick, date, -num_shares)
+            pass
 
     #Calculates portfolio net profit 
     def calc_Net_Profit(self):
@@ -155,39 +238,32 @@ class Portfolio:
                     st.visualize_Stock()
         pass
 
-line = None
-pf = Portfolio()
+def main():
+    line = None
+    pf = Portfolio()
 
-#Continuously asks for stock input until user is done
-while line != "Done":
-    print("Enter a stock in the format Format: \"Ticker Share_Count YYYY-MM-DD\"")
-    line = input("Stock: ")
+    #Continuously asks for stock input until user is done
+    while True:
+        print("Enter a stock in the format Format: 'Ticker Share_Count YYYY-MM-DD' or enter 'Done'.")
+        line = input("Stock: ")
 
-    if line.lower() == "done":
-        break
+        if line.lower() == "done":
+            break
 
-    #Fetches tick + shares from input 
-    stock_input = line.split()
-    tick = stock_input[0].upper()
-    share_count = stock_input[1]
-    invest_datetime = dt.strptime(stock_input[2], "%Y-%m-%d")
-    
-    #Gets stock data from yfinance
-    tick_data = yf.download(tick)
-    # print(tick_data)
+        #Fetches tick + shares from input 
+        stock_input = line.split()
+        tick = stock_input[0].upper()
+        shares = float(stock_input[1])
+        date = stock_input[2]
+        
+        #Creates stock object and adds to portfolio
+        pf.add_Stock(tick, date, shares)
+        pf.print_Transactions()
 
-    #Check if date is valid in data
-    while not invest_datetime.strftime("%Y-%m-%d") in tick_data.iloc[:, 0] :
-        invest_datetime += timedelta(days = 1)
-        #print(f"Date added changed to {}")
+    pf.visualize_Profits()
 
-    #Creates stock object and adds to portfolio
-    stock = Stock(tick, invest_datetime, tick_data, share_count)
-    pf.add_Stock(stock)
-    pf.print_Transactions()
+    pf.process_Request()
 
-pf.visualize_Profits()
-
-pf.process_Request()
+main()
     
 
