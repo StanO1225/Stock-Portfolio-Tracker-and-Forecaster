1fa4dd324566fac56cdf830ba1d40a01a085ae22
Stanley Ou <stanleyo@andrew.cmu.edu>
2024-05-31 00:15:18 -0400

    Created working webpage, stock profits function

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template, request
+import portfolio_tracker as pt
+import yfinance as yf
+
+global pf 
+global hasGraph
 
 app = Flask(__name__)
+pf = pt.Portfolio()
 
-@app.route("/")
-def index():
-    return "Bruh"
+@app.route("/", methods=["GET", "POST"])
+def main():
+    hasGraph = False
+
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        tick = data["tick"].upper()
+        date = data["date"]
+        shares = float(data["shares"])
+
+        pf.add_Stock(tick, date, shares)
+        hasGraph = True
+
+        
+
+    return render_template("index.html", transactions= pf.getTransactions(), ifGraph = hasGraph)
 
 app.run(host = "0.0.0.0", port = 80)
--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -14,15 +14,8 @@ import seaborn as sns
 from datetime import timedelta, datetime as dt
 from modeling import get_Predictions
 from typing import List, Dict, Tuple, Union, Optional, Any, Callable, Iterable
+from plotnine import ggplot, geom_bar
 
-
-#Class definitions
-# Stock Class:
-"""
-ticker - ticker tag string
-date_added - datetime object
-
-"""
 class Stock:
     def __init__(self, ticker: str, date_shares: List[Tuple[object, float]], data: object):
         self.ticker = ticker 
@@ -45,58 +38,106 @@ class Stock:
 
         return res
     
-    def get_Start_Value(self) -> List[float]:
+    def getPurchasePrices(self, date: object=None) -> List[float]:
         """
-        Gets the prices of the stock when purchased iniitally at each date. 
+        Returns a list of prices of the stock when purchased iniitally at each date. If date specified, only returns prices 
+        of shares bought on or before the date.  
         """
         res = []
-        for share_count, date_added in self.date_shares:
-            res.append(float(self.data.loc[date_added].iloc[3]) * float(share_count))
+
+        if date:
+            while not date.strftime("%Y-%m-%d") in self.data.iloc[:, 0] :
+                date -= timedelta(days = 1)
+            
+        for date_added, share_count in self.date_shares:
+            if date:
+                if date_added <= date:
+                    res.append(float(self.data.loc[date_added].iloc[3]) * float(share_count))
+            else:
+                res.append(float(self.data.loc[date_added].iloc[3]) * float(share_count))
+
         return res
 
-    def get_Curr_Value(self) -> List[float]:
+    def getPriceAtDate(self, date=None) -> List[float]:
         """
-        Gets current price of the stock, given number of shares. 
+        Gets current total price of the stock at a given date. 
         """
+        if date:
+            while not date.strftime("%Y-%m-%d") in self.data.iloc[:, 0] :
+                date += timedelta(days = 1)
+            price = float(self.data.loc[date]["Close"])   
 
-        curr_price = float(self.data.iloc[-1][3])   
-        return curr_price * self.getShares()
+        else:
+            price = float(self.data.iloc[-1][3])   
 
-    def get_Profit(self) -> float:
+        return price * self.getShares()
+
+    def netProfit(self, date=None) -> float:
         """
-        Returns net profit/loss from this stock. 
+        Returns net profit/loss from this stock from dates of investment to date given. 
         """
         shares = [share_count for _, share_count in self.date_shares]
 
-        sum = 0
-        for v, s in zip(self.get_Start_Value(), shares):
-            res += v*s
+        investments = 0
 
-        return sum - self.get_Curr_Value()
-    
+        for v in self.getPurchasePrices():
+            investments += v
+
+        return self.getPriceAtDate(date) - investments # Current total value of stocks - Total invested initially 
+
     def addShares(self, date: str, num_shares) -> None:
+        """
+        Adds shares to the stock given a date. 
+        """
         self.date_shares.append((dt.strptime(date, "'%Y-%m-%d'"), num_shares))
     
     def getShares(self) -> float:
+        """
+        Returns the total number of shares of this stock. 
+        """
         return sum([share_count for _, share_count in self.date_shares])
+    
+    def getDates(self) -> List[object]:
+        """
+        Returns list of dates when stock was purchased. 
+        """
+        return [dt for dt, _ in self.date_shares]
+    
+    def profitsData(self):
+        """
+        Refactors the stock data, used for plotting. 
+        """
+
+        min_date = min(self.getDates())
+            
+
+        profit_df = pd.DataFrame()
+
+        profit_df["Date"] = self.data.reset_index()[self.data.reset_index()["Date"] >= min_date]["Date"]
+        profit_df["Profit"] = profit_df["Date"].apply(self.netProfit)
+
+        return profit_df
     
     def visualize_Stock(self) -> None:
         """
         Creates line graph of stock's performance.
         """
         dat = self.data.reset_index()
+        start = min(self.getDates())
 
         plt.title(f"{self.ticker} Stock Performance")
-        sns.lineplot(x="Date", y="Close", data = dat[dat["Date"] >= self.date_added.strftime('%Y-%m-%d')],
+
+        sns.lineplot(x="Date", y="Close", data = dat[dat["Date"] >= start.strftime('%Y-%m-%d')],
                      err_style="bars")
-        plt.show()
+
+        plt.savefig(r"C:\Users\stano\Documents\Projects\Stock Portfolio\static\stock.jpg")
         pass
 
     def make_Predictions(self) -> float:
         return get_Predictions(self.data)
 
 class Portfolio:
-    def __init__(self, stock_list = [], transactions = "", net_worth = 0):
+    def __init__(self, stock_list = [], transactions = [], net_worth = 0):
         self.stock_list = stock_list # List of stock objects 
         # self.inactive_stocks = []
         self.transactions = transactions # List of transactions 
@@ -110,7 +151,7 @@ class Portfolio:
         """
         stock_ticker = stock_ticker.upper()
         for st in self.stock_list:
-            if st.ticker == stock_ticker :
+            if st.ticker == stock_ticker: 
                 return st
         return None
     
@@ -123,8 +164,13 @@ class Portfolio:
             st.date_shares.append((invest_dt, num_shares))
             pass
     
+        try: 
+            tick_data = yf.download(tick)
+        except:
+            print("Invalid Stock")
+            pass
+
         #Gets stock data from yfinance
-        tick_data = yf.download(tick)
         # print(tick_data)
 
         #Check if date is valid in data
@@ -133,11 +179,18 @@ class Portfolio:
             #print(f"Date added changed to {}")
 
         stock = Stock(tick, [(invest_dt, num_shares)], tick_data)   
+
+        stock.visualize_Stock()
+
+        print("Before")
+        print(stock.profitsData())
+        print("After")
+
         self.stock_list.append(stock)
 
-        self.transactions += ("\n" + str(stock))
+        self.transactions.append(str(stock))
 
-        self.net_worth += stock.get_Curr_Value()
+        self.net_worth += stock.netProfit()
 
         pass
 
@@ -170,7 +223,7 @@ class Portfolio:
     def calc_Net_Profit(self):
         profit = 0
         for st in self.stock_list:
-            profit += st.get_Profit()
+            profit += st.netProfit()
         return profit
     
     def update_Net_Worth(self) -> None:
@@ -181,36 +234,47 @@ class Portfolio:
 
     #Visualizes the total profit from this portfolio over time (starting from earliest stock in portfolio)
     def visualize_Profits(self):
-        profits_df = pd.DataFrame()
-        for i, stock in enumerate(self.stock_list):
-            dat = stock.data
-            stock_profits = dat[stock.date_added : dt.now()].drop(dat.columns.difference(['Close']), axis=1)
-            stock_profits.columns = [stock.ticker]
-            stock_profits[stock.ticker] = (stock_profits[stock.ticker]*float(stock.share_count) - stock.get_Start_Value()) 
-            if i == 0:
-                profits_df = stock_profits
-            else:
-                if len(profits_df.index) < len(stock_profits.index):
-                    profits_df = stock_profits.join(profits_df, on=stock_profits.index)
-                else:
-                    profits_df = profits_df.join(stock_profits, on=profits_df.index)
-        profits_df['Total Profits'] = profits_df.sum(axis=1, numeric_only=True)
-        profits_df['Positive?'] = profits_df['Total Profits'] >= 0
-        print(profits_df)
+        # profits_df = pd.DataFrame()
+        # for i, stock in enumerate(self.stock_list):
+        #     dat = stock.data
+        #     stock_profits = dat[stock.date_added : dt.now()].drop(dat.columns.difference(['Close']), axis=1)
+        #     stock_profits.columns = [stock.ticker]
+        #     stock_profits[stock.ticker] = (stock_profits[stock.ticker]*float(stock.share_count) - stock.get_Start_Value()) 
+        #     if i == 0:
+        #         profits_df = stock_profits
+        #     else:
+        #         if len(profits_df.index) < len(stock_profits.index):
+        #             profits_df = stock_profits.join(profits_df, on=stock_profits.index)
+        #         else:
+        #             profits_df = profits_df.join(stock_profits, on=profits_df.index)
+        # profits_df['Total Profits'] = profits_df.sum(axis=1, numeric_only=True)
+        # profits_df['Positive?'] = profits_df['Total Profits'] >= 0
+        # print(profits_df)
         
-        plt.style.use('ggplot')
-        plt.figure(figsize=(16,12))
-        plt.title("Portfolio Net Profit by Time")
-        graph_ = sns.lineplot(data=profits_df, x=profits_df.index, y="Total Profits", c='Green')
-        graph_.xaxis.set_major_locator(ticker.LinearLocator(5))
-        plt.savefig("Portfolio_Profits.jpg")
-        plt.show()
+        # plt.style.use('ggplot')
+        # plt.figure(figsize=(16,12))
+        # plt.title("Portfolio Net Profit by Time")
+        # graph_ = sns.lineplot(data=profits_df, x=profits_df.index, y="Total Profits", c='Green')
+        # graph_.xaxis.set_major_locator(ticker.LinearLocator(5))
+        # plt.savefig("Portfolio_Profits.jpg")
+        # plt.show()
 
         pass
 
-    def print_Transactions(self):
-        print(self.transactions)
+    def graphLine():
         pass
+
+    def graphProfits():
+        pass
+
+    def graphInvestments():
+        pass
+
+    def graphBreakdown():
+        pass
+
+    def getTransactions(self):
+        return self.transactions
 
     def process_Request(self):
         while (1):
@@ -225,7 +289,7 @@ class Portfolio:
                     st = self.get_stock(req)
                     print(st.make_Predictions())
                 case "history":
-                    self.print_Transactions()
+                    print(self.getTransactions())
                 case "profit":
                     print("$ " + str(self.calc_Net_Profit()))
                 case "q":
@@ -238,7 +302,7 @@ class Portfolio:
                     st.visualize_Stock()
         pass
 
-def main():
+def run():
     line = None
     pf = Portfolio()
 
@@ -258,12 +322,19 @@ def main():
         
         #Creates stock object and adds to portfolio
         pf.add_Stock(tick, date, shares)
-        pf.print_Transactions()
+        pf.getTransactions()
 
-    pf.visualize_Profits()
+    # pf.visualize_Profits()
 
     pf.process_Request()
 
-main()
-    
+pf = Portfolio()
+
+pf.add_Stock("AAPL", "2024-04-30", 1)
+
+pf.add_Stock("AMZN", "2023-01-01", 1)
+
+
+# df = yf.download("AAPL")
+# print(df.head())
 
